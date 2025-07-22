19dc65caab1aafb42535dce80361aa243aeb5f9b
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-07-22 10:52:42 -0300

    required args constructor added

--- libraryapi/src/main/java/io/github/educastrob/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/io/github/educastrob/libraryapi/service/AutorService.java
@@ -6,6 +6,7 @@ import io.github.educastrob.libraryapi.model.Livro;
 import io.github.educastrob.libraryapi.repository.AutorRepository;
 import io.github.educastrob.libraryapi.repository.LivroRepository;
 import io.github.educastrob.libraryapi.validator.AutorValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,17 +14,19 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class AutorService {
 
     private final AutorRepository repository;
     private final AutorValidator validator;
     private final LivroRepository livroRepository;
 
-    public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository) {
-        this.repository = repository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
+    // ---> Não precisa por conta da annotation @RequiredArgsConstructor
+//    public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository) {
+//        this.repository = repository;
+//        this.validator = validator;
+//        this.livroRepository = livroRepository;
+//    }
 
     public Autor salvar(Autor autor) {
         validator.validar(autor);
